e51deca36c2584c44a90d74a358d941689cb8865
mrc <marcoambrosini@pm.me>
2019-02-12 15:12:07 +0100

    Updates

---
+++ precache-manifest.dc01853277ca385edda6eaf36615b933.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "24cfdf39d0a47d4a3ee6",
+    "url": "/random-quote-machine/static/css/main.e0c80723.chunk.css"
+  },
+  {
+    "revision": "24cfdf39d0a47d4a3ee6",
+    "url": "/random-quote-machine/static/js/main.24cfdf39.chunk.js"
+  },
+  {
+    "revision": "9f17e117bf3add37571c",
+    "url": "/random-quote-machine/static/js/1.9f17e117.chunk.js"
+  },
+  {
+    "revision": "b8507572375d5b5ea7f9",
+    "url": "/random-quote-machine/static/js/runtime~main.b8507572.js"
+  },
+  {
+    "revision": "69e7036ef247a0d2a17707655ebf2315",
+    "url": "/random-quote-machine/index.html"
+  }
+];
