e981d08dfa8f5bc01c3e633d18b501fb5964d304
mrc <marcoambrosini@pm.me>
2019-02-14 16:42:11 +0100

    fixed scroll for mobile

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -2,7 +2,10 @@ import React from "react"
 
 function Component(props) {
   return (
-    <div className="quote">
+    <div
+      className="quote"
+      style={{ "overflow -y": "auto", "max-height": "100%" }}
+    >
       <h1 id="text">“ {props.quote} ”</h1>
       <h4 id="author">{props.author}</h4>
     </div>
