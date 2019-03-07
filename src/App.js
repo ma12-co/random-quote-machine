0af1d0f0960df9e8c76fa84e8efcf54a79ca2a00
mrc <marcoambrosini@pm.me>
2019-03-07 08:54:12 +0100

    light mode default

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default class App extends Component {
       quotes: {},
       quote: "",
       author: "",
-      darkMode: true,
+      darkMode: false,
       fullScreen: false,
       scheduler: {
         hourBased: {
